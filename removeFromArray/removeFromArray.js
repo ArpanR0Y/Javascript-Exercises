b5bae522d18ec3ad0836420734dfdc83cb37a408
Arpan Roy <arpanroy.dev@gmail.com>
2021-01-20 15:05:41 +0530

    ✔

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...element) {
+  for (let i = 0; i < element.length; i++) {
+    removeElementFromArray(arr, element[i]);
+  }
+  return arr;
+};
 
+function removeElementFromArray(arr, element) {
+  if (arr.includes(element)) {
+    let indexOfElement = arr.indexOf(element);
+    arr.splice(indexOfElement, 1);
+  }
 }
 
-module.exports = removeFromArray
+module.exports = removeFromArray;
