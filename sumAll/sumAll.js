0a5a94b7591eed8ecb520c5810db53c23aa98a00
Arpan Roy <arpanroy.dev@gmail.com>
2021-01-21 07:03:17 +0530

    ✔

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (typeof start != "number" || typeof end != "number") {
+        return "ERROR";
+    } else if (start < 0 || end < 0) {
+        return "ERROR";
+    } else {
+        if (start > end) {
+            let temp;
+            temp = start;
+            start = end;
+            end = temp;
+        }
+        let total = 0;
+        for (let i = start; i <= end ; i++) {
+            total += i;
+        }
+        return total;
+    }
 }
 
 module.exports = sumAll
